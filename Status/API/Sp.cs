6b4cad87fcbe64d9e0a06aa585855b3aff127851
mayurshinde0106 <officialmayur0106@gmail.com>
2024-07-19 18:24:31 +0530

    Update Sp.cs

---
+++ Status/API/Sp.cs
@@ -0,0 +1,16 @@
+    #region login 
+    public const string getloginprocedures = "[dbo].[getloginprocedure]";
+    public const string insertloginprocedures = "[dbo].[insertloginprocedure]";
+    public const string getuser = "[dbo].[getuser]";
+    #endregion
+
+    #region Task
+    public const string inserttaskprocedure = "[dbo].[inserttaskprocedure]";
+    public const string getalltasksprocedure = "[dbo].[getalltasksprocedure]";
+    public const string gettaskbyidprocedure = "[dbo].[gettaskbyidprocedure]";
+    public const string deletetaskbyidprocedure = "[dbo].[deletetaskbyidprocedure]";
+    public const string updatetaskbyidprocedure = "[dbo].[updatetaskbyidprocedure]";
+    public const string gettaskbystatus = "[dbo].[gettaskbystatus]";
+    public const string getcountbystatus = "[dbo].[countbystatus]";
+    #endregion
+}
